7164344f8f172e69d0e57dff2016bfb541c00ace
Chip Bell <cbell@pbs.org>
2016-09-30 11:23:35 -0400

    Adding an object/configuration validator

--- src/config-validator.js
+++ src/config-validator.js
@@ -1,3 +1,9 @@
+/**
+ * Gets a nested object value for a particular key
+ * @param {Object} object The object to get a value from
+ * @param {String} key The object key to get
+ * @return {Object} Could be anything
+ */
 const get = function (object, key) {
   const tokens = key.split('.');
   var pointer = object;
@@ -12,6 +18,56 @@ const get = function (object, key) {
   return pointer;
 };
 
+const validateSingle = function (value, type) {
+  if (type === 'Array') {
+    return value instanceof Array;
+  } else if (type === 'String') {
+    return typeof value === 'string';
+  } else if (type === 'Number') {
+    return typeof value === 'number';
+  } else if (type === 'Boolean') {
+    return typeof value === 'boolean';
+  }
+
+  throw new Error('Cannot validate against unsupported type ' + type);
+};
+
+/**
+ * Validates an object based on a series of validation rules provided. Returns an array of error
+ * messages. If the array is empty, the validation was successful.
+ *
+ * Example ruleset might be:
+ * {
+ *   'A' : {
+ *     type: 'Boolean',
+ *     message: 'A is not a boolean'
+ *   },
+ *   'B' : {
+ *     type: 'String',
+ *     message: 'B should be a string'
+ *   },
+ *   'C.D' : { // a nested key!
+ *     type: 'Number',
+ *     message: 'D should be a number'
+ *   }
+ * }
+ *
+ * @param {Object} object The object to validate
+ * @param {Object} rules The list of validation rules to use
+ * @return {Array}
+ */
+const validate = function (object, rules) {
+  return Object.keys(rules)
+    .filter((key) => {
+      var value = get(object, key);
+      return !validateSingle(value, rules[key].type);
+    })
+    .map((key) => {
+      return rules[key].message;
+    });
+};
+
 module.exports = {
-  get
+  get,
+  validate
 };
--- test/config-validator.test.js
+++ test/config-validator.test.js
@@ -27,4 +27,91 @@ describe('config-validator', () => {
       expect(configValidator.get(obj, 'A.B.C')).toEqual(undefined);
     });
   });
+
+  describe('validate', () => {
+    it('should be able to validate a string', () => {
+      var badObject = { key: 10 };
+      var goodObject = { key: 'ten' };
+      var rules = {
+        'key' : {
+          type: 'String',
+          message: 'not a string'
+        }
+     };
+
+      expect(configValidator.validate(badObject, rules)).toEqual(['not a string']);
+      expect(configValidator.validate(goodObject, rules)).toEqual([]);
+    });
+
+    it('should be able to validate a number', () => {
+      var badObject = { key: 'ten' };
+      var goodObject = { key: 10 };
+      var rules = {
+        'key': {
+          type: 'Number',
+          message: 'not a number'
+        }
+      };
+
+      expect(configValidator.validate(badObject, rules)).toEqual(['not a number']);
+      expect(configValidator.validate(goodObject, rules)).toEqual([]);
+    });
+
+    it('should be able to validate a boolean', () => {
+      var badObject = { key: 'ten' };
+      var goodObject = { key: true };
+      var rules = {
+        'key': {
+          type: 'Boolean',
+          message: 'not a bool'
+        }
+      };
+
+      expect(configValidator.validate(badObject, rules)).toEqual(['not a bool']);
+      expect(configValidator.validate(goodObject, rules)).toEqual([]);
+    });
+
+    it('should be able to validate an Array', () => {
+      var badObject = { key: 'nope' };
+      var goodObject = { key: [1, 2, 3] };
+      var rules = {
+        'key' : {
+          type: 'Array',
+          message: 'not an array'
+        }
+      };
+
+      expect(configValidator.validate(badObject, rules)).toEqual(['not an array']);
+      expect(configValidator.validate(goodObject, rules)).toEqual([]);
+    });
+
+    it('should return an array of validation errors that fail', () => {
+      var rules = {
+        A : {
+          type: 'Number',
+          message: 'not a number'
+        },
+        B : {
+          type: 'String',
+          message: 'not a string'
+        },
+        'C.D' : {
+          type: 'Boolean',
+          message: 'c.d. fails'
+        }
+      };
+
+      var object = {
+        A: 7,
+        B: false,
+        C: {
+          D: true
+        }
+      };
+
+      var result = configValidator.validate(object, rules);
+      expect(result.length).toEqual(1);
+      expect(result[0]).toEqual(['not a string']);
+    });
+  });
 });
