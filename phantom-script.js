6dfcd4506152ccbb2fa9286b258d8fab66e2ac24
Chip Bell <cbell@pbs.org>
2017-06-11 20:25:50 -0400

    Initial working test script that doesn't use mocha-phantom

---
+++ phantom-script.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(message) {
+  console.log(message);
+};
+
+page.onCallback = function(data) {
+  var status = 0;
+  if(data.failingTests && data.failingTests !== 0) {
+    console.log(data.failingTests, 'tests failed');
+    status = 1;
+  }
+  phantom.exit(status);
+};
+
+page.onLoadFinished = function() {
+  var runTests = function() {
+    window.mocha.run(function(failingTests) {
+      window.callPhantom({ failingTests: failingTests });
+    });
+  };
+
+  page.evaluateJavaScript(runTests.toString());
+  setTimeout(function() {
+    phantom.exit(0);
+  }, 5000);
+};
+
+var path = 'file://' + fs.workingDirectory + '/test/index.html';
+page.open(path);
