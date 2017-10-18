d89cc728152eaaaf40344027c1593fe74844533e
Chip Bell <cbell@pbs.org>
2017-10-18 12:15:14 -0400

    Fixing a build error

--- gulp/bundle.js
+++ gulp/bundle.js
@@ -2,6 +2,7 @@ const browserify = require('browserify');
 const gulp = require('gulp');
 const source = require('vinyl-source-stream');
 const streamify = require('gulp-streamify');
+const uglify = require('gulp-uglify');
 
 module.exports = (entryPoint, outputFilename, outputDestinations, debug) => {
   // default options for debug mode
