b3bd73a09456b5c42bdd1b518359c3848487e85a
Chip Bell <cbell@pbs.org>
2017-10-18 13:56:44 -0400

    Separating map files from dist code

--- gulp/bundle.js
+++ gulp/bundle.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const source = require('vinyl-source-stream');
 const streamify = require('gulp-streamify');
 const uglify = require('gulp-uglify');
+const exorcist = require('exorcist');
 
 module.exports = (entryPoint, outputFilename, outputDestinations, debug) => {
   // default options for debug mode
@@ -11,6 +12,7 @@ module.exports = (entryPoint, outputFilename, outputDestinations, debug) => {
   var inProcessBundle = browserify(entryPoint, { debug: debug })
     .transform('babelify')
     .bundle()
+    .pipe(exorcist('dist/' + outputFilename + '.map'))
     .pipe(source(outputFilename));
 
   if(!debug) {
