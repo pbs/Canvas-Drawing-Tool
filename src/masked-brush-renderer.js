d56cbda17e3360e50c3fa085fd33f616f683c5d7
Chip Bell <cbell@pbs.org>
2017-12-05 15:19:33 -0500

    Pencil Eraser now showing preview path

--- src/masked-brush-renderer.js
+++ src/masked-brush-renderer.js
@@ -3,8 +3,11 @@
  */
 const MaskedBrushRenderer = {
   _render: function () {
-    var ctx = this.canvas.contextTop;
-    ctx.strokeStyle = 'white';
+    var background = this.canvas.wrapperEl.previousElementSibling;
+    var backgroundPattern = this.canvas.contextTop.createPattern(background, 'no-repeat');
+
+    this.canvas.contextTop.strokeStyle = backgroundPattern;
+
     this.callSuper('_render');
   }
 };
